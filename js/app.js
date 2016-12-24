6edcbaf0b274816f8326456655cd36e4f1bc4b52
Rashmi Malpande <rashmimalpande@gmail.com>
2016-12-24 14:09:22 +0530

    Add functions to increase & decrease length

--- js/app.js
+++ js/app.js
@@ -3,7 +3,29 @@ var app = angular.module('app', []);
 app.controller('TimeCtrl', ['$scope',
      function($scope){
          $scope.message = "Hello World";
-    
+         $scope.breakLength = 5;
+         $scope.sessionLength = 25;
+         $scope.decreaseBreak = function(){
+             if($scope.breakLength != 0){
+                 $scope.breakLength--;
+             }
+             
+         }
+
+         $scope.increaseBreak = function(){
+             $scope.breakLength++;
+         }
+
+         $scope.decreaseSession = function() {
+             if($scope.breakLength != 0){
+                $scope.sessionLength--;
+             }
+             
+         }
+
+         $scope.increaseSession = function() {
+             $scope.sessionLength++;
+         }
     }
 
 ]);
