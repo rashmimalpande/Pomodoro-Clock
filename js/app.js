d3f6014b6d0e448cd3d834b1591a3102b3823926
Rashmi Malpande <rashmimalpande@gmail.com>
2017-01-03 17:58:40 +0530

    Fill up background color on click

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ app.controller('TimeCtrl', ['$scope',
          $scope.sessionLength = 25;
          $scope.minutes = $scope.sessionLength;
          $scope.seconds = 0;
+         $scope.fillerHeight = 0;
+         $scope.start = false;
+         $scope.stop = false;
+
          $scope.decreaseBreak = function(){
              if($scope.breakLength > 1){
                  $scope.breakLength--;
@@ -31,7 +35,21 @@ app.controller('TimeCtrl', ['$scope',
 
 
          $scope.toggleTimer = function() {
+             if($scope.fillerHeight === 0){
+                 $scope.start = true;
+                 $scope.fillerHeight = 100;
+             }
+             
+             else if($scope.fillerHeight > 0 && $scope.start){
+                 $scope.start = false;
+                 $scope.stop = true;
+             }
 
+             else if($scope.fillerHeight > 0 && $scope.stop){
+                 $scope.stop = false;
+                 $scope.start = true;
+                 
+             }
          }
     }
 
