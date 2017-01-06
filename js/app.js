188ae597419251fcde2070b740e3e4bda6a1552a
Rashmi Malpande <rashmimalpande@gmail.com>
2017-01-06 10:44:40 +0530

    Start & stop background color timer on click

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ var app = angular.module('app', []);
 
 app.controller('TimeCtrl', ['$scope','$interval',
      function($scope, $interval){
-         $scope.message = "Hello World";
          $scope.breakLength = 5;
          $scope.sessionLength = 25;
          $scope.minutes = $scope.sessionLength;
          $scope.seconds = 60;
+         $scope.timeLeft = 0;
          $scope.fillerHeight = 0;
          $scope.color = "#99CC00";
          $scope.start = false;
@@ -36,27 +36,38 @@ app.controller('TimeCtrl', ['$scope','$interval',
 
 
          $scope.toggleTimer = function() {
-             
-             $interval(function(){
-                    if($scope.minutes !== 0){
-                        $scope.minutes--;
-                    }                                
-            }, 1000);
+            if($scope.fillerHeight === 0){
+                $scope.start = true;
+                $scope.startTimer();
 
-             if($scope.fillerHeight === 0){
-                 $scope.start = true;
-                 $scope.fillerHeight = 100;
-             }
-             else if($scope.fillerHeight > 0 && $scope.start){
-                 $scope.start = false;
-                 $scope.stop = true;
-                 
-             }
-             else if($scope.fillerHeight > 0 && $scope.stop){
-                 $scope.start = true;
-                 $scope.stop = false;
-             }
+            }
+
+            else if($scope.fillerHeight > 0 && $scope.start){
+                $scope.start = false;
+                $scope.stop = true;
+                $scope.stopTimer();
+            }
+
+            else if($scope.fillerHeight > 0 && $scope.stop){
+                $scope.start = true;
+                $scope.stop = false;
+                $scope.startTimer();
+            }
+            
              
+         };
+
+         $scope.startTimer = function(){
+             $scope.timeLeft = $interval(function(){
+                if($scope.fillerHeight!= 100){
+                    $scope.fillerHeight++;
+                }
+
+            },1000);
+         };
+
+         $scope.stopTimer = function(){
+             $interval.cancel($scope.timeLeft);
          };
 
       
